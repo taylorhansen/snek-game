255ea22b99c8b1a4270781c905dd26cf79cc8a37
CrazyGuy108 <taylor@hansenfamily.us>
2019-09-06 19:37:31 -0700

    Add head torque controls

--- snake/Snake.gd
+++ snake/Snake.gd
@@ -1,17 +1,58 @@
 extends Node2D
 
+const ROTATE_FORCE = 1000
+const SEGMENT_SCALING = 0.75
+
 # tail segment
 onready var _tail: SnakeSegment = $Tail
+onready var _seg2: SnakeSegment = $SnakeSegment2
+onready var _seg3: SnakeSegment = $SnakeSegment3
+onready var _seg4: SnakeSegment = $SnakeSegment4
 # head segment
 onready var _head: SnakeSegment = $Head
 
 func _input(event: InputEvent):
-    # execute grip actions
+    # head grip
     if event.is_action_pressed("head_grip"):
         _head.grip()
     elif event.is_action_released("head_grip"):
         _head.ungrip()
+    # head left
+    if event.is_action_pressed("head_left"):
+        _head_left()
+    elif event.is_action_released("head_left"):
+        _head_stop()
+    # head right
+    if event.is_action_pressed("head_right"):
+        _head_right()
+    elif event.is_action_released("head_right"):
+        _head_stop()
+    # tail grip
     elif event.is_action_pressed("tail_grip"):
         _tail.grip()
     elif event.is_action_released("tail_grip"):
         _tail.ungrip()
+
+func _head_left():
+    if not _tail.is_touching_wall() or not _tail.is_gripping():
+        _head_stop()
+        return
+    # apply a force to each segment excluding tail
+    var force = ROTATE_FORCE
+    for seg in [_seg2, _seg3, _seg4, _head]:
+        seg.rotate_left(force)
+        force *= SEGMENT_SCALING
+
+func _head_right():
+    if not _tail.is_touching_wall() or not _tail.is_gripping():
+        _head_stop()
+        return
+    # apply a force to each segment excluding tail
+    var force = ROTATE_FORCE
+    for seg in [_seg2, _seg3, _seg4, _head]:
+        seg.rotate_right(force)
+        force *= SEGMENT_SCALING
+
+func _head_stop():
+    for seg in [_seg2, _seg3, _seg4, _head]:
+        seg.rotate_left(0)
--- snake/SnakeSegment.gd
+++ snake/SnakeSegment.gd
@@ -1,21 +1,41 @@
 extends RigidBody2D
 class_name SnakeSegment
 
+onready var _height = $CollisionShape2D.shape.height
+
 # wall bodies currently touching this segment
 var _walls: Array = []
+var _torque: float = 0
+
+func rotate_left(force: float):
+    _torque = -force
+
+func rotate_right(force: float):
+    _torque = force
 
 # grips this segment to a wall by changing into a static body
 func grip():
-    if _is_touching_wall():
+    if is_touching_wall():
         mode = MODE_STATIC
+
+func is_gripping():
+    return mode == MODE_STATIC
 
 # ungrips this segment from a wall by changing back into a rigid body
 func ungrip():
     mode = MODE_RIGID
 
 # checks if we're touching a wall body
-func _is_touching_wall() -> bool:
+func is_touching_wall() -> bool:
     return len(_walls) > 0
+
+func _ready():
+    set_physics_process(true)
+
+func _physics_process(delta):
+    # apply torque forces
+    if _torque != 0:
+        apply_impulse(Vector2(0, -_height / 2), Vector2(_torque, 0) * delta)
 
 func _on_SnakeSegment_body_entered(body: Node):
     if body is StaticBody2D:
