90d4cd716de156a0a61437faaadcb78c18784070
CrazyGuy108 <taylor@hansenfamily.us>
2019-09-08 15:00:38 -0700

    Add confetti and yay sound when basket has ball

--- basket/Basket.gd
+++ basket/Basket.gd
@@ -2,6 +2,16 @@ extends StaticBody2D
 
 signal ball_entered
 
+onready var _yay: = $Yay
+onready var _green_confetti: = $GreenConfetti
+onready var _red_confetti: = $RedConfetti
+
 func _on_BallDetector_body_entered(body: PhysicsBody2D):
     if body.is_in_group("ball"):
-        emit_signal("ball_entered")
+        _yay.play()
+        _green_confetti.emitting = true
+        _red_confetti.emitting = true
+
+# emit the game over signal after the basket congratulates the player
+func _on_Yay_finished():
+    emit_signal("ball_entered")
