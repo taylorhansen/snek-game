3686ab117fd28060cc420c3425d3ecf23b3e4a61
CrazyGuy108 <taylor@hansenfamily.us>
2019-09-07 19:19:03 -0700

    Remove hit/thump sounds for now
    Didn't sound very well with overall gameplay, may come back to this
    later if there's a better way to play/design those sounds.

--- snake/SnakeSegment.gd
+++ snake/SnakeSegment.gd
@@ -6,15 +6,11 @@ onready var _height = $CollisionShape2D.shape.height
 
 onready var _grab_sound: AudioStreamPlayer2D = $GrabSound
 onready var _grab_release_sound: AudioStreamPlayer2D = $GrabReleaseSound
-onready var _hit_sound: AudioStreamPlayer2D = $HitSound
-onready var _thump_sound: AudioStreamPlayer2D = $ThumpSound
 
 # current torque value
 var _torque: float = 0
 # whether to apply the rotational force to the top or the bottom
 var _top: bool = true
-# queued collision processing for a contacted body
-var _contact_body = null
 
 # grips this segment to a wall by changing into a static body
 func grip():
@@ -68,30 +64,3 @@ func _physics_process(delta):
         var impulse: Vector2 = force * delta
         apply_impulse(offset, -impulse)
         apply_impulse(-offset, impulse)
-
-func _on_SnakeSegment_body_entered(body: Node):
-    # register wall contacts for collision processing in _integrate_forces
-    if body.is_in_group("wall"):
-        _contact_body = body
-
-func _integrate_forces(state: Physics2DDirectBodyState):
-    if _contact_body != null:
-        # search contacts for the recently collided body
-        for i in range(state.get_contact_count()):
-            var obj = state.get_contact_collider_object(i)
-            if _contact_body == obj:
-                # get contact normal and our velocity at the point of contact
-                var normal = state.get_contact_local_normal(i)
-                var vel = state.linear_velocity
-                # vector projection of velocity onto collision normal
-                # don't need to include normal.length_squared() since it's
-                #  guaranteed to be 1
-                var proj = normal.dot(vel) * normal
-                # play a sound based on how "hard" the collision was
-                var len2 = proj.length_squared()
-                print("normal ", normal, " vel ", vel, " proj ", proj, " len2 ",
-                    len2)
-                #if len2 > 200: _thump_sound.play()
-                #else: _hit_sound.play()
-                break
-        _contact_body = null
