24d2be9acb74cedc07c5f6d9c5711a8679ed2304
CrazyGuy108 <taylor@hansenfamily.us>
2019-09-07 20:36:23 -0700

    Fix scene switcher not loading new scene

--- global/Global.gd
+++ global/Global.gd
@@ -1,12 +1,16 @@
 extends Node
 
+onready var root: = get_tree().get_root()
+onready var current_scene: = root.get_child(root.get_child_count() - 1)
+
 # queues a scene change at the end of the process loop
 func goto_scene(scene: Resource):
     call_deferred("_goto_scene_deferred", scene)
 
 # initiates a scene change
 func _goto_scene_deferred(scene: Resource):
-    var tree = get_tree()
     # now in idle time so it's ok to delete the current scene
-    tree.current_scene.free()
-    tree.current_scene = scene.instance()
+    root.remove_child(current_scene)
+    current_scene.free()
+    current_scene = scene.instance()
+    root.add_child(current_scene)
