7bf842e2121c7f90f88e85621c688026ebfe9745
CrazyGuy108 <taylor@hansenfamily.us>
2019-09-08 23:35:25 -0700

    Change particle colors

--- basket/Basket.gd
+++ basket/Basket.gd
@@ -7,13 +7,13 @@ signal game_over
 
 onready var _yay: = $Yay
 onready var _green_confetti: = $GreenConfetti
-onready var _red_confetti: = $RedConfetti
+onready var _brown_confetti: = $BrownConfetti
 
 func _on_BallDetector_body_entered(body: PhysicsBody2D):
     if body.is_in_group("ball"):
         _yay.play()
         _green_confetti.emitting = true
-        _red_confetti.emitting = true
+        _brown_confetti.emitting = true
         emit_signal("ball_entered")
 
 # emit the game over signal after the basket congratulates the player
